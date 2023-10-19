230c7827077450605b8a802440e7902818338ac6
vhanla <vhanla@gmail.com>
2023-10-19 18:14:28 -0500

    🆕 Add Fast Open With GUI

---
+++ FastOpenWith.Main.dfm
@@ -0,0 +1,15 @@
+object mainForm: TmainForm
+  Left = 0
+  Top = 0
+  Caption = 'Fast Open With...'
+  ClientHeight = 442
+  ClientWidth = 628
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poScreenCenter
+  TextHeight = 15
+end
---
+++ FastOpenWith.Main.pas
@@ -0,0 +1,24 @@
+unit FastOpenWith.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TmainForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  mainForm: TmainForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
