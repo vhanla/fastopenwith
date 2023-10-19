a0fdeefc3d7304d9e37e4bdd1e48055add6baafa
vhanla <vhanla@gmail.com>
2023-10-19 16:04:26 -0500

    🆕 Add ActiveX COM for Shell Extension

---
+++ FastOpenWith_TLB.pas
@@ -0,0 +1,59 @@
+unit FastOpenWith_TLB;
+
+// ************************************************************************ //
+// WARNING
+// -------
+// The types declared in this file were generated from data read from a
+// Type Library. If this type library is explicitly or indirectly (via
+// another type library referring to this type library) re-imported, or the
+// 'Refresh' command of the Type Library Editor activated while editing the
+// Type Library, the contents of this file will be regenerated and all
+// manual modifications will be lost.
+// ************************************************************************ //
+
+// $Rev: 98336 $
+// File generated on 19/10/2023 03:28:34 p. m. from Type Library described below.
+
+// ************************************************************************  //
+// Type Lib: S:\Projects\Delphi\fastopenwith\FastOpenWithShell (1)
+// LIBID: {56342785-7401-40B7-BCA7-6887BA4D35AD}
+// LCID: 0
+// Helpfile:
+// HelpString:
+// DepndLst:
+//   (1) v2.0 stdole, (C:\Windows\SysWOW64\stdole2.tlb)
+//   (2) v4.0 StdVCL, (C:\Windows\SysWow64\stdvcl40.dll)
+// SYS_KIND: SYS_WIN32
+// ************************************************************************ //
+{$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers.
+{$WARN SYMBOL_PLATFORM OFF}
+{$WRITEABLECONST ON}
+{$VARPROPSETTER ON}
+{$ALIGN 4}
+
+interface
+
+uses Winapi.Windows, System.Classes, System.Variants, System.Win.StdVCL, Vcl.Graphics, Winapi.ActiveX;
+
+
+// *********************************************************************//
+// GUIDS declared in the TypeLibrary. Following prefixes are used:
+//   Type Libraries     : LIBID_xxxx
+//   CoClasses          : CLASS_xxxx
+//   DISPInterfaces     : DIID_xxxx
+//   Non-DISP interfaces: IID_xxxx
+// *********************************************************************//
+const
+  // TypeLibrary Major and minor versions
+  FastOpenWithMajorVersion = 1;
+  FastOpenWithMinorVersion = 0;
+
+  LIBID_FastOpenWith: TGUID = '{56342785-7401-40B7-BCA7-6887BA4D35AD}';
+
+
+implementation
+
+uses System.Win.ComObj;
+
+end.
+
