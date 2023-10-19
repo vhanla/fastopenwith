e113bf4990063cd93b4f1b2d18e07625c08748d4
vhanla <vhanla@gmail.com>
2023-10-19 17:43:43 -0500

    🛠️ Update project settings

--- FastOpenWith.dpr
+++ FastOpenWith.dpr
@@ -1,10 +1,12 @@
 library FastOpenWith;
 
-{$R *.dres}
+
+{.$R *.dres}
 
 uses
   ComServ,
-  FastOpenWith_TLB in 'FastOpenWith_TLB.pas';
+  FastOpenWith_TLB in 'FastOpenWith_TLB.pas',
+  FastOpenWithMenu in 'FastOpenWithMenu.pas';
 
 exports
   DllGetClassObject,
